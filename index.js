57772b02341d893b9b22c0e91721c988a4da5ad6
Erik Huang <ekhu94@gmail.com>
2021-04-20 15:26:16 -0700

    create movie schema and create a new model using Mongoose

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ mongoose
   })
   .then(() => console.log("Connection Open"))
   .catch((err) => console.log(err));
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+const yourName = new Movie({
+  title: "君の名は",
+  year: 2016,
+  score: 8.4,
+  rating: "PG",
+});
